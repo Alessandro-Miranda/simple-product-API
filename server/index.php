2c994f05cbfde968bc396b9cefdc4c5ed0c97c48
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-20 17:02:57 -0300

    Atualizado o index

--- server/index.php
+++ server/index.php
@@ -1,3 +1,23 @@
 <?php
-    require 'bootstrap.php';
+    require 'vendor/autoload.php';
+
+    use App\Controller\ProductController;
+    use Dotenv\Dotenv;
+
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->safeLoad();
+
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $uri = explode('/', $uri);
+
+    if($uri[1] != 'products')
+    {
+        header("HTTP/1.1 404 Not Found");
+        exit();
+    }
+
+    $queryString = $_SERVER['QUERY_STRING'];
+
+    new ProductController($queryString);
 ?>
