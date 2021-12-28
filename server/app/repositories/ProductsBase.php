5570aa89b371064a8c106f08196b07c18993fcd2
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-28 15:03:00 -0300

    Criado o insert dos produtos'

---
+++ server/app/repositories/ProductsBase.php
@@ -0,0 +1,76 @@
+<?php
+    namespace App\Repositories;
+
+    use App\Utils\RegisterLog;
+use Error;
+use Exception;
+    use PDO;
+
+    class ProductsBase extends Database
+    {
+        function __construct()
+        {
+            parent::__construct();
+            echo "inserindo os produtos no banco de dados";
+        }
+
+        public function insert($values, $tableName)
+        {
+            try
+            {
+                $this->PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->PDO->beginTransaction();
+
+                foreach($values as $actualItem)
+                {
+                    $lenght = count($actualItem);
+                    $counter = 0;
+                    $columns = "";
+                    $columnValues = "";
+
+                    array_walk($actualItem, function($item, $key) use (&$columns, &$columnValues, &$counter, &$lenght) {
+                        
+                        if($counter >= $lenght - 1)
+                        {
+                            $columns .= $key;
+                            $columnValues .= gettype($item) === "string" ? "'{$item}'" : $item;
+                        }
+                        else
+                        {
+                            $columns .= "{$key},";
+                            $columnValues .= gettype($item) === "string" ? "'{$item}'," : "{$item},";
+                        }
+
+                        $counter++;
+                    });
+
+                    $stmt = "INSERT INTO {$tableName} ({$columns}) VALUES ({$columnValues})";
+                    
+                    $this->PDO->exec($stmt);
+                }
+
+                if($this->PDO->commit())
+                {
+                    RegisterLog::RegisterLog("Insert completion", "Produtos inseridos no banco", "Insert-infos.log");
+                    echo "Produtos baixados <br />";
+                }
+                else
+                {
+                    echo "Ocorreu um erro <br />";
+                    throw new Error("Erro ao commitar a transação");
+                }
+
+            }
+            catch(Exception $err)
+            {
+                RegisterLog::RegisterLog(
+                    "Database Exception (Insert Transaction)",
+                    $err->getMessage(),
+                    "exceptions.log"
+                );
+
+                $this->PDO->rollBack();
+            }
+        }
+    }
+?>
