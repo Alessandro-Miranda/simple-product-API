7226cd0eece80acb9734a7aa3d1ffcdfd71bada8
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-28 15:03:23 -0300

    Atualizado o formato da hora no registro do log

--- server/app/utils/RegisterLog.php
+++ server/app/utils/RegisterLog.php
@@ -1,11 +1,15 @@
 <?php
     namespace App\Utils;
-    
-    class RegisterLog
+
+use DateTime;
+use DateTimeZone;
+
+class RegisterLog
     {
         public static function RegisterLog($infoLevel, $message, $fileName)
         {
-            $date = date('l jS \of F Y h:i:s A');
+            $date = new DateTime("now", new DateTimeZone('America/Sao_Paulo'));
+            $date = $date->format('l jS \of F Y h:i:s A');
             $msg = "{$date} - " . strtoupper($infoLevel) . " - Message: {$message} \n";
 
             file_put_contents($fileName, $msg, FILE_APPEND);
