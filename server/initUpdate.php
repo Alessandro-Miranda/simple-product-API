ffbef2a38b94573b066393288a24276fef19c603
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-03 16:51:57 -0300

    Criado a inicialização da atualização dos produtos

---
+++ server/initUpdate.php
@@ -0,0 +1,36 @@
+<?php
+    use App\Model\UpdateProducts;
+    use Dotenv\Dotenv;
+
+    require 'vendor/autoload.php';
+
+    if(PHP_SAPI === 'cli')
+    {
+        $dotenvValues = str_replace("\"", "", file_get_contents('.env'));
+        $exploadedValues = array_filter(explode("\r\n", $dotenvValues));
+
+        foreach($exploadedValues as $value)
+        {
+            if(preg_match('/\s|#^\d/' , $value))
+            {
+                continue;
+            }
+
+            $envArray = explode("=", $value);
+            
+            $_ENV[$envArray[0]] = $envArray[1];
+        }
+    }
+    else
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->safeLoad();
+    }
+
+    $update = new UpdateProducts();
+
+    $update
+        ->getPriceInformations()
+        ->getProductInformations()
+        ->update();
+?>
