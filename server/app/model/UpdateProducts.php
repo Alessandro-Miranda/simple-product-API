2362ee856a121519fe353a9f08761747069d27c0
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-04 09:48:53 -0300

    Incluído a instanciação da classe Database

--- server/app/model/UpdateProducts.php
+++ server/app/model/UpdateProducts.php
@@ -2,20 +2,17 @@
 namespace App\Model;
 
 use App\Repositories\Database;
-use App\Repositories\UpdateProducts as RepositoriesUpdateProducts;
 
 class UpdateProducts extends DownloadProducts
 {
     function __construct()
     {
         parent::__construct();
-
-        $this->db = new Database();
     }
 
     public function update()
     {
-        $db = new RepositoriesUpdateProducts();
-        $db->update($this->products);
+        $db = new Database();
+        $db->updateProducts($this->products);
     }
 }
