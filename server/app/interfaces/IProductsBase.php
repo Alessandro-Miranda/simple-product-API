79968532129722df56f71f53adf6f26df2259bd0
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-04 09:44:38 -0300

    Criado a interface da manipulação do download de produtos

---
+++ server/app/interfaces/IProductsBase.php
@@ -0,0 +1,13 @@
+<?php
+
+interface IProductsBase
+{
+    /**
+     * Realiza a inserção dos produtos no banco
+     *
+     * @param array  $values
+     * @param string $tableName
+     * @return void
+     */
+    public function insertProducts(array $values, string $tableName): void;
+}
