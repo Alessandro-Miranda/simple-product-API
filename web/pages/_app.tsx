e55ba99ab5f71b5dbbb9cbcbb28192bed7872dbe
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-12 11:37:55 -0300

    Removido a pasta que seria utilizada para criação de interface de exemplo

--- web/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- web/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return (
-        <div>
-            <p>Aqui vai o conteúdo da page</p>
-        </div>
-    );
-}
-
-export default Home;
